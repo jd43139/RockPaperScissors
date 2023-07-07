3deaaeb6ca4759aef1635f4a9243b0c7dc44b7d8
jd43139 <109301496+jd43139@users.noreply.github.com>
2023-07-07 09:49:29 +0400

    Create getComputerChoice() and playerSelection functions

---
+++ script.js
@@ -0,0 +1,11 @@
+function getComputerChoice(){
+    const options = ["rock", "paper", "scissors"]
+    let random = Math.floor(Math.random()*3)
+    return options[random];
+}
+function playerSelection(){
+    let playerChoice=prompt("Please type Rock, Paper, or Scissors","Rock")
+    return playerChoice.toLowerCase()
+}
+
+
