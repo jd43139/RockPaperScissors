a77db4f363c7d7b124316a8ef7915a3d22427794
jd43139 <109301496+jd43139@users.noreply.github.com>
2023-07-08 10:56:45 +0400

    Create game() function

--- script.js
+++ script.js
@@ -3,9 +3,44 @@ function getComputerChoice(){
     let random = Math.floor(Math.random()*3)
     return options[random];
 }
-function playerSelection(){
+function getPlayerChoice(){
     let playerChoice=prompt("Please type Rock, Paper, or Scissors","Rock")
     return playerChoice.toLowerCase()
 }
+let computerScore=0
+let playerScore=0
+function playRound(computerSelection, playerSelection){
+    
+    if(computerSelection==playerSelection){
+        console.log("Tied!")
+    }else{
+        if(computerSelection=="rock" && playerSelection=="scissors"){
+            computerScore+=1
+            console.log("You Lose")
+        }else if(computerSelection=="rock" && playerSelection=="paper"){
+            playerScore+=1
+            console.log("You Win")
+        }else if(computerSelection=="scissors" && playerSelection=="paper"){
+            computerScore+=1
+            console.log("You Lose")
+        }else if(computerSelection=="scissors" && playerSelection=="rock"){
+            playerScore+=1
+            console.log("You Win")
+        }else if(computerSelection=="paper" && playerSelection=="rock"){
+            computerScore+=1
+            console.log("You Lose")
+        }else{
+            playerScore+=1
+            console.log("You Win")
+        }
+    }
+}
 
-function playRound()
+function game(){
+    let rounds=5
+    while(rounds>0){
+        playRound()
+        rounds-=1
+    }
+}
+
