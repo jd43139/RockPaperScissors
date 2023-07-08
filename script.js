554d0c7977533b6b207731ff31fe4cfc5450aaad
jd43139 <109301496+jd43139@users.noreply.github.com>
2023-07-08 11:52:00 +0400

    Display the final result after the final round

--- script.js
+++ script.js
@@ -12,36 +12,54 @@ let playerScore=0
 function playRound(computerSelection, playerSelection){
     
     if(computerSelection==playerSelection){
-        return `Tied!\n Computer: ${computerSelection} Player: ${playerSelection}`
+        return `Tied!\n Computer: ${computerSelection} Player: ${playerSelection}\n
+    Computer Score: ${computerScore} Player Score: ${playerScore}`
     }else{
         if(computerSelection=="rock" && playerSelection=="scissors"){
             computerScore+=1
-            return `You Lose\n Computer: ${computerSelection} Player: ${playerSelection}`
+            return `You Lose\n Computer: ${computerSelection} Player: ${playerSelection}\n
+        Computer Score: ${computerScore} Player Score: ${playerScore}`
         }else if(computerSelection=="rock" && playerSelection=="paper"){
             playerScore+=1
-            return `You Win\n Computer: ${computerSelection} Player: ${playerSelection}`
+            return `You Win\n Computer: ${computerSelection} Player: ${playerSelection}\n
+        Computer Score: ${computerScore} Player Score: ${playerScore}`
         }else if(computerSelection=="scissors" && playerSelection=="paper"){
             computerScore+=1
-            return `You Lose\n Computer: ${computerSelection} Player: ${playerSelection}`
+            return `You Lose\n Computer: ${computerSelection} Player: ${playerSelection}\n
+        Computer Score: ${computerScore} Player Score: ${playerScore}`
         }else if(computerSelection=="scissors" && playerSelection=="rock"){
             playerScore+=1
-            return `You Win\n Computer: ${computerSelection} Player: ${playerSelection}`
+            return `You Win\n Computer: ${computerSelection} Player: ${playerSelection}\n
+        Computer Score: ${computerScore} Player Score: ${playerScore}`
         }else if(computerSelection=="paper" && playerSelection=="rock"){
             computerScore+=1
-            return `You Lose\n Computer: ${computerSelection} Player: ${playerSelection}`
+            return `You Lose\n Computer: ${computerSelection} Player: ${playerSelection}\n
+        Computer Score: ${computerScore} Player Score: ${playerScore}`
         }else{
             playerScore+=1
-            return `You Win\n Computer: ${computerSelection} Player: ${playerSelection}`
+            return `You Win\n Computer: ${computerSelection} Player: ${playerSelection}\n
+        Computer Score: ${computerScore} Player Score: ${playerScore}`
         }
     }
 }
+function finalResult(){
+    if(playerScore==computerScore){
+        return `DRAW\nComputer Score: ${computerScore} | Player Score: ${playerScore}`
+    }else{
+        if(playerScore<computerScore){
+            return `You LOST\nComputer Score: ${computerScore} | Player Score: ${playerScore}`
+        }else{
+            return `YOU WON\nComputer Score: ${computerScore} | Player Score: ${playerScore}`
+        }
+    }
 
+}
 function game(){
     let rounds=5
     while(rounds>0){
         console.log(playRound(getComputerChoice(),getPlayerChoice()))
         rounds-=1
     }
-    console.log(playerScore, computerScore)
+    console.log(finalResult())
 }
 game()
