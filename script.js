aabe7eae301804acf46d093b81e9a81571acb430
jd43139 <109301496+jd43139@users.noreply.github.com>
2023-07-09 12:58:35 +0400

    Hanlde unexpected inputs

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ function getComputerChoice(){
     let random = Math.floor(Math.random()*3)
     return options[random];
 }
+let playerChoice=""
 function getPlayerChoice(){
-    let playerChoice=prompt("Please type Rock, Paper, or Scissors","Rock")
+    playerChoice=prompt("Please type Rock, Paper, or Scissors","Rock")
     return playerChoice.toLowerCase()
 }
 let computerScore=0
@@ -12,31 +13,31 @@ let playerScore=0
 function playRound(computerSelection, playerSelection){
     
     if(computerSelection==playerSelection){
-        return `Tied!\nComputer chose: ${computerSelection} | You chose: ${playerSelection}\nComputer Score: ${computerScore} | Player Score: ${playerScore}`
+        return `Tied!\n Computer: ${computerSelection} Player: ${playerSelection}\nComputer Score: ${computerScore} Player Score: ${playerScore}`
     }else{
         if(computerSelection=="rock" && playerSelection=="scissors"){
             computerScore+=1
-            return `You Lose\nComputer chose: ${computerSelection} | You chose: ${playerSelection}\nComputer Score: ${computerScore} | Player Score: ${playerScore}`
-
+            return `You Lose\n Computer: ${computerSelection} Player: ${playerSelection}\n
+        Computer Score: ${computerScore} Player Score: ${playerScore}`
         }else if(computerSelection=="rock" && playerSelection=="paper"){
             playerScore+=1
-            return `You Win\nComputer chose: ${computerSelection} | You chose: ${playerSelection}\nComputer Score: ${computerScore} | Player Score: ${playerScore}`
-
+            return `You Win\nComputer: ${computerSelection} Player: ${playerSelection}\n
+        Computer Score: ${computerScore} Player Score: ${playerScore}`
         }else if(computerSelection=="scissors" && playerSelection=="paper"){
             computerScore+=1
-            return `Computer chose: ${computerSelection} | You chose: ${playerSelection}\nComputer Score: ${computerScore} | Player Score: ${playerScore}`
-
+            return `You Lose\nComputer: ${computerSelection} Player: ${playerSelection}\n
+        Computer Score: ${computerScore} Player Score: ${playerScore}`
         }else if(computerSelection=="scissors" && playerSelection=="rock"){
             playerScore+=1
-            return `You Win\nComputer chose: ${computerSelection} | You chose: ${playerSelection}\nComputer Score: ${computerScore} | Player Score: ${playerScore}`
-
+            return `You Win\n Computer: ${computerSelection} Player: ${playerSelection}\n
+        Computer Score: ${computerScore} Player Score: ${playerScore}`
         }else if(computerSelection=="paper" && playerSelection=="rock"){
             computerScore+=1
-            return `You Lose\nComputer chose: ${computerSelection} | You chose: ${playerSelection}\nComputer Score: ${computerScore} | Player Score: ${playerScore}`
-
+            return `You Lose\nComputer: ${computerSelection} Player: ${playerSelection}\nComputer Score: ${computerScore} Player Score: ${playerScore}`
         }else{
             playerScore+=1
-            return `You Win\nComputer chose: ${computerSelection} | You chose: ${playerSelection}\nComputer Score: ${computerScore} | Player Score: ${playerScore}`
+            return `You Win\n Computer: ${computerSelection} Player: ${playerSelection}\n
+        Computer Score: ${computerScore} Player Score: ${playerScore}`
         }
     }
 }
@@ -45,18 +46,33 @@ function finalResult(){
         return `DRAW\nComputer Score: ${computerScore} | Player Score: ${playerScore}`
     }else{
         if(playerScore<computerScore){
-            return `You LOST\nComputer Total Score: ${computerScore} | Player Total Score: ${playerScore}`
+            return `You LOST\nComputer Score: ${computerScore} | Player Score: ${playerScore}`
         }else{
-            return `YOU WON\nComputer Total Score: ${computerScore} | Player Total Score: ${playerScore}`
+            return `YOU WON\nComputer Score: ${computerScore} | Player Score: ${playerScore}`
         }
+    }
+
+}
+function isValidAnswer(){
+    playerChoice=getPlayerChoice()
+    if(playerChoice=="rock" || playerChoice=="paper" || playerChoice=="scissors"){
+        return true
+    }else{
+        return false
     }
 
 }
 function game(){
     let rounds=5
     while(rounds>0){
-        console.log(playRound(getComputerChoice(),getPlayerChoice()))
-        rounds-=1
+        const computerChoice=getComputerChoice()
+        if(isValidAnswer()){
+            console.log(playRound(computerChoice,playerChoice))
+            rounds-=1
+        }else{
+            alert("Type a valid choice")
+        }
+        
     }
     console.log(finalResult())
 }
