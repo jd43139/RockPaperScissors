82c3943a47da5ff77678bda04095ac088b48cbd9
jd43139 <109301496+jd43139@users.noreply.github.com>
2023-07-10 12:05:03 +0400

    Make the results more organised

--- script.js
+++ script.js
@@ -10,45 +10,42 @@ function getPlayerChoice(){
 }
 let computerScore=0
 let playerScore=0
+
+
 function playRound(computerSelection, playerSelection){
     
     if(computerSelection==playerSelection){
-        return `Tied!\n Computer: ${computerSelection} Player: ${playerSelection}\nComputer Score: ${computerScore} Player Score: ${playerScore}`
+        return `Tied!\nComputer selected: ${computerSelection} You selected: ${playerSelection}\nComputer current Score: ${computerScore} | Your Current Score: ${playerScore}`
     }else{
         if(computerSelection=="rock" && playerSelection=="scissors"){
             computerScore+=1
-            return `You Lose\n Computer: ${computerSelection} Player: ${playerSelection}\n
-        Computer Score: ${computerScore} Player Score: ${playerScore}`
+            return `You Lose\nComputer selected: ${computerSelection} You selected: ${playerSelection}\nComputer current Score: ${computerScore} | Your Current Score: ${playerScore}`
         }else if(computerSelection=="rock" && playerSelection=="paper"){
             playerScore+=1
-            return `You Win\nComputer: ${computerSelection} Player: ${playerSelection}\n
-        Computer Score: ${computerScore} Player Score: ${playerScore}`
+            return `You Win\nComputer selected: ${computerSelection} You selected: ${playerSelection}\nComputer current Score: ${computerScore} | Your Current Score: ${playerScore}`
         }else if(computerSelection=="scissors" && playerSelection=="paper"){
             computerScore+=1
-            return `You Lose\nComputer: ${computerSelection} Player: ${playerSelection}\n
-        Computer Score: ${computerScore} Player Score: ${playerScore}`
+            return `You Lose\nComputer selected: ${computerSelection} You selected: ${playerSelection}\nComputer current Score: ${computerScore} | Your Current Score: ${playerScore}`
         }else if(computerSelection=="scissors" && playerSelection=="rock"){
             playerScore+=1
-            return `You Win\n Computer: ${computerSelection} Player: ${playerSelection}\n
-        Computer Score: ${computerScore} Player Score: ${playerScore}`
+            return `You Win\nComputer selected: ${computerSelection} You selected: ${playerSelection}\nComputer current Score: ${computerScore} | Your Current Score: ${playerScore}`
         }else if(computerSelection=="paper" && playerSelection=="rock"){
             computerScore+=1
-            return `You Lose\nComputer: ${computerSelection} Player: ${playerSelection}\nComputer Score: ${computerScore} Player Score: ${playerScore}`
+            return `You Loose\nComputer selected: ${computerSelection} You selected: ${playerSelection}\nComputer current Score: ${computerScore} | Your Current Score: ${playerScore}`
         }else{
             playerScore+=1
-            return `You Win\n Computer: ${computerSelection} Player: ${playerSelection}\n
-        Computer Score: ${computerScore} Player Score: ${playerScore}`
+            return `You Win\nComputer selected: ${computerSelection} You selected: ${playerSelection}\nComputer current Score: ${computerScore} | Your Current Score: ${playerScore}`
         }
     }
 }
 function finalResult(){
     if(playerScore==computerScore){
-        return `DRAW\nComputer Score: ${computerScore} | Player Score: ${playerScore}`
+        return `DRAW\nComputer Total Score: ${computerScore} | Your Total Score: ${playerScore}`
     }else{
         if(playerScore<computerScore){
-            return `You LOST\nComputer Score: ${computerScore} | Player Score: ${playerScore}`
+            return `You LOST\nComputer Total Score: ${computerScore} | Your Total Score: ${playerScore}`
         }else{
-            return `YOU WON\nComputer Score: ${computerScore} | Player Score: ${playerScore}`
+            return `YOU WON\nComputer Total Score: ${computerScore} | Your  Total Score: ${playerScore}`
         }
     }
 
